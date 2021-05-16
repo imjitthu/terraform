66bd684c6739418e974002903f2d86d8f6be5a78
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-16 15:24:31 +0530

    roboshop

--- frontend/Frontend.tf
+++ frontend/Frontend.tf
@@ -42,11 +42,11 @@ provisioner "remote-exec" {
 
 
 output "EC2_Public_IP" {
-    value = aws_spot_instance_request.Frontend.public_ip
+    value = aws_spot_instance_request.frontend.public_ip
     description = "Publisc IP of WorkStation"
 }
 output "EC2_instance_id" {
-  value       = aws_spot_instance_request.Frontend.id
+  value       = aws_spot_instance_request.frontend.id
   description = "EC2 Instance ID"
 }
 resource "aws_route53_record" "frontend" {
