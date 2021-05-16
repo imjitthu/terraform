2b598a3de62ba295fed9a4ecb4dcaf4573fcf3b3
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-16 12:19:06 +0530

    roboshop

--- workstation/workstation.tf
+++ workstation/workstation.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "WorkStation" {
-ami = "ami-079a3f3cf00741286"
-instance_type = "${var.INSTANCE_TYPE}"
-tags = {
-  "name" = "${var.COMPONENT}"
-}
-}
+# resource "aws_instance" "WorkStation" {
+# ami = "ami-079a3f3cf00741286"
+# instance_type = "${var.INSTANCE_TYPE}"
+# tags = {
+#   "name" = "${var.COMPONENT}"
+# }
+# }
 
 resource "aws_route53_record" "WorkStation" {
   zone_id = "${var.R53_ZONE_ID}"
