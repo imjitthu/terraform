468b77f742ed0f708b1a1645671669f4ee2ffbec
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-16 10:17:23 +0530

    roboshop

--- workstation/workstation.tf
+++ workstation/workstation.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "var.ec2_instances" {
+resource "aws_instance" "WorkStation" {
 ami = "ami-079a3f3cf00741286"
 instance_type = "t2.small"
 tags = {
-  "name" = "var.ec2_instances"
+  "name" = "WorkStation"
 }
 }
 
