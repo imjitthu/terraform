691432474c78a941a0b65add70af7f7eacbfeeac
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-16 12:37:22 +0530

    roboshop

--- workstation/workstation.tf
+++ workstation/workstation.tf
@@ -1,24 +1,18 @@
 resource "aws_instance" "Work_Station" {
 ami = "ami-079a3f3cf00741286"
 instance_type = "${var.INSTANCE_TYPE}"
+associate_public_ip_address = true
 tags = {
   "name" = "${var.COMPONENT}"
 }
 }
 resource "aws_route53_record" "WorkStation" {
-  zone_id = "Z077254017HKF6MBGS2JG"
-  name = "workstation.jithendar.com"
+  zone_id = "${var.R53_ZONE_ID}"
+  name = "workstation.${var.DOMAIN}"
   type = "A"
   ttl = "300"
-  records = [ "aws_instance.WorkStation.public_ip" ]
+  records = [ "aws_instance.${var.COMPONENT}.public_ip" ]
 }
-# resource "aws_route53_record" "WorkStation" {
-#   zone_id = "${var.R53_ZONE_ID}"
-#   name = "workstation.${var.DOMAIN}"
-#   type = "A"
-#   ttl = "300"
-#   records = [ "aws_instance.WorkStation.public_ip" ]
-# }
 
 output "EC2_Details" {
     value = "aws_instance.${var.COMPONENT}.public_ip"
