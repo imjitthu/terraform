8d7937b1c9dc1fb10f1c4c4a681fcd77c4aca5c8
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-16 14:15:27 +0530

    roboshop

--- frontend/workstation.tf
+++ frontend/workstation.tf
@@ -7,21 +7,34 @@ spot_type = one-time
 tags = {
   "name" = "${var.COMPONENT}"
 }
+
+provisioner "file" {
+    source      = "script.sh"
+    destination = "/tmp/script.sh"
+  }
+
+provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/script.sh",
+      "/tmp/script.sh args",
+    ]
+  }
+
 }
 
 output "EC2_Public_IP" {
-    value = aws_instance.WorkStation.public_ip
+    value = aws_instance.Frontend.public_ip
     description = "Publisc IP of WorkStation"
 }
 output "EC2_instance_id" {
-  value       = aws_instance.WorkStation.id
+  value       = aws_instance.Frontend.id
   description = "EC2 Instance ID"
 }
 
-resource "aws_route53_record" "WorkStation" {
+resource "aws_route53_record" "Frontend" {
   zone_id = "${var.R53_ZONE_ID}"
   name = "workstation.${var.DOMAIN}"
   type = "A"
   ttl = "300"
-  records = [ aws_instance.WorkStation.public_ip ]
+  records = [ aws_instance.Frontend.public_ip ]
 }
