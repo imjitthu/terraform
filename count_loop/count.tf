25894e9760202801351ec6f7d855aeffc37bf130
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 09:37:30 +0530

    roboshop

--- count_loop/count.tf
+++ count_loop/count.tf
@@ -7,9 +7,12 @@ resource "aws_vpc" "VPC_Count" {
 }
 
 resource "aws_subnet" "subnet-1" {
+  availability_zone = "${length(data.aws_availability_zones.AV_ZONES.names, count.index)}"
+  #count = "${length(var.AV_ZONES)}"
+  count = "${length(data.aws_availibility_zones.AV_ZONES.names)}"
   vpc_id = "${aws_vpc.test-VPC-count.id}"
-  cidr_block = "${var.SUBNET_CIDR}"
+  cidr_block = "${element(var.SUBNET_CIDR, count.index)}"
   tags = {
-    Name = "subnet-1-Count"
+    Name = "subnet-1-Count-${count.index+1}"
   }
 }
--- count_loop/vars.tf
+++ count_loop/vars.tf
@@ -5,3 +5,20 @@ variable "REGION" {
 variable "VPC_CIDR" { default = "190.160.0.0/16"}
 
 variable "SUBNET_CIDR" {}
+
+#Manual Availability Zones List
+# variable "AV_Zones" {
+#     type = "list"
+#     default = [
+#         "us-east-1a", 
+#         "us-east-1b",
+#         "us-east-1c",
+#         "us-east-1d",
+#         "us-east-1e",
+#         "us-east-1f", 
+#         ]
+# }
+
+#Get Availability Zones from DataSources
+data "aws_availability_zones" "AV_ZONES" {}
+
