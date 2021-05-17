8f2dbf2a2acc60d794e965276cc5348e8dacafa0
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 11:58:11 +0530

    roboshop

--- rs-instacses/instances.tf
+++ rs-instacses/instances.tf
@@ -23,6 +23,6 @@ provisioner "remote-exec" {
 }
 
 output "instances" {
-  value = aws_instance.instances[each.value].public_ip
+  value = aws_instance.instances[each.key].public_ip
   description = "Publuc IPs"
 }
