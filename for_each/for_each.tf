6805bcbf8785d4a989de06a190989a8617baa731
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 11:01:43 +0530

    roboshop

--- for_each/for_each.tf
+++ for_each/for_each.tf
@@ -1,8 +1,12 @@
 resource "aws_vpc" "VPC_FOR_EACH" {
-    for_each = toset(var.CIDR_FOR_EACH)
+    for_each = var.CIDR_MAP
     cidr_block = each.value
-  
+  tags = {
+    Name = each.key
+  }
 }
 
 
 # for_each (meta argument) helps in creating multiple resources with multiple values at single time.
+
+# toset used for list variable ex: for_each = toset(var.CIDR_LIST)
--- for_each/vars.tf
+++ for_each/vars.tf
@@ -1,5 +1,12 @@
-variable "CIDR_FOR_EACH" {
-    #type = list(string)
+variable "CIDR_LIST" {
     default = ""
     description = "list variable for CIDRs"
+}
+
+variable "CIDR_MAP" {
+    default = {
+        vpc_1 = "10.0.0.0/16",
+        vpc_2 =  "10.2.0.0/16",
+    }
+    description = "Map variable for CIDRs"
 }
