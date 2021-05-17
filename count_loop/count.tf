5e683850b6910e0d216ab60d9b8c1ae464743f0d
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 09:10:50 +0530

    roboshop

--- count_loop/count.tf
+++ count_loop/count.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "VPC_Count" {
-    cidr_block = "190.160.0.0/16"
+    cidr_block = "${var.VPC_CIDR}"
     instance_tenancy = "default"
     tags = {
       Name = "test-VPC-Count"
@@ -8,7 +8,7 @@ resource "aws_vpc" "VPC_Count" {
 
 resource "aws_subnet" "subnet-1" {
   vpc_id = "${aws_vpc.test-VPC-count.id}"
-  cidr_block = "190.160.1.0/24"
+  cidr_block = "${var.SUBNET_CIDR}"
   tags = {
     Name = "subnet-1-Count"
   }
--- count_loop/vars.tf
+++ count_loop/vars.tf
@@ -1,3 +1,7 @@
 variable "REGION" {
     default = "us-east-1"
 }
+
+variable "VPC_CIDR" { default = "190.160.0.0/16"}
+
+variable "SUBNET_CIDR" {}
