9d1adf8eb6be1bba9135f672d01cda8267152334
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 15:07:30 +0530

    roboshop

--- frontend/provider.tf
+++ frontend/provider.tf
@@ -1,4 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = "/root/.aws/credentials"
+  access_key_ID = AKIAZV7JD5PGN4LFY5GQ 
+  secret_access_key = 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
 }
