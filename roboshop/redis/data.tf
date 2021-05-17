f1594e22dcadfdf2a00a5d904ba0690c31ace444
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-17 20:08:53 +0530

    roboshop

--- roboshop/redis/data.tf
+++ roboshop/redis/data.tf
@@ -1,8 +1,7 @@
-data "terraform_remote_state" "rs-projecy" {
-  backend           = "s3"
-  config            = {
-    bucket          = "terraform-state-jithendar"
-    key             = "rs-instances/terraform.tfstate"
-    region          = "us-east-1"
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-jithendar"
+    key    = "rs-instances/${var.COMPONENT}.tfstate"
+    region = "us-east-1"
   }
 }
