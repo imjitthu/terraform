875818287632b3e97b89f2f22f65f18546ca9451
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-19 15:24:48 +0530

    roboshop

--- roles/frontend/main.tf
+++ roles/frontend/main.tf
@@ -44,7 +44,7 @@ provisioner "remote-exec" {
 
 
 provisioner "local-exec" {
-  command = "cd /; ansible-playbook -i ${aws_instance.frontend.private_ip}, -u root -K ${var.PASSWORD} frontend.yml"
+  command = "cd /; ansible-playbook -i ${aws_instance.frontend.private_ip}, -u root -p ${var.PASSWORD} frontend.yml"
 }
 }
 
