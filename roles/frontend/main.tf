d5a7310a3d83017de1b904d0d3f3c61679c512e6
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-19 16:40:21 +0530

    roboshop

--- roles/frontend/main.tf
+++ roles/frontend/main.tf
@@ -1,16 +1,16 @@
-# resource "aws_instance" "frontend" {
-#   # aws_spot_instabce_request for spot instance
-#   ami = "${var.AMI}"
-#   instance_type = "${var.INSTANCE_TYPE}"
-#   # spot_type = "one-time"  aws_spot_instance_request
-#   tags = {
-#     "Name" = "${var.COMPONENT}-Server"
-#   }
+resource "aws_instance" "frontend" {
+  # aws_spot_instabce_request for spot instance
+  ami = "${var.AMI}"
+  instance_type = "${var.INSTANCE_TYPE}"
+  # spot_type = "one-time"  aws_spot_instance_request
+  tags = {
+    "Name" = "${var.COMPONENT}-Server"
+  }
 
 provisioner "local-exec" {
-  command = "pwd; ls; ansible-playbook -i 3.237.21.106, --private-key=test.pem ${var.COMPONENT}.yml"
+  command = "ansible-playbook -i ${aws_instance.frontend.private_ip}, --private-key=test.pem ${var.COMPONENT}.yml"
 }
-# }
+}
 
 resource "aws_route53_record" "frontend" {
   zone_id = "${var.R53_ZONE_ID}"
