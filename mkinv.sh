a98b783981d221da0116fddc7942b43c8ac3b933
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 16:04:54 +0530

    roboshop

--- mkinv.sh
+++ mkinv.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 >inv
-for component in frontend catalogue cart user shipping payment mysql mongo redis rabbitmq; do
+for component in *; do
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress' |xargs)
   if [ -n "${IP}" ]; then
     echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv
