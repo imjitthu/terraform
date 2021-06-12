23889c9701c2a119caedbd492cf07b57878b9873
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 14:12:13 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -17,10 +17,10 @@ output "Component" {
 }
 
 resource "aws_route53_record" "roboshop" {
-  count = length(aws_instance.instance)
+  count = length(var.COMPONENT)
   allow_overwrite = true
   zone_id    = data.aws_route53_zone.jithendar.zone_id
-  name       = "${element(aws_instance.instance, count.index)}".data.aws_route53_zone.jithendar.name
+  name       = "${element(var.COMPONENT, count.index)}".data.aws_route53_zone.jithendar.name
   type       = "A"
   ttl        = "300"
   records    = ["192.168.0.1"]
