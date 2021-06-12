dd581da92ec67b31b6f5dc6c881e9114ab0558f2
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 14:22:18 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -8,30 +8,13 @@ resource "aws_instance" "instance" {
   }
 }
 
-output "ami" {
-  value = aws_instance.instance[*].private_ip
-}
-
-output "Component" {
-  value = var.COMPONENT
-}
-
 resource "aws_route53_record" "roboshop" {
   count = length(var.COMPONENT)
   allow_overwrite = true
   name       = "${element(var.COMPONENT, count.index)}.${data.aws_route53_zone.jithendar.name}"
   zone_id    = data.aws_route53_zone.jithendar.zone_id
-  #.data.aws_route53_zone.jithendar.name
   type       = "A"
   ttl        = "300"
   records    = ["${element(aws_instance.instance, count.index)}".private_ip]
 }
-
-# resource "aws_route53_record" "jithendar" {
-#   name          = "${var.COMPONENT}.${data.aws_route53_zone.jithendar.name}"
-#   type          = "A"
-#   ttl           = "300"
-#   zone_id       = data.aws_route53_zone.jithendar.zone_id
-#   records       = [aws_instance.cart.private_ip]
-# }
 
