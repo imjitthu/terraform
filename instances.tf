127361d9ba7ac72d11c37ebce5f2c363503f30a1
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 14:27:33 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   count   = length(var.COMPONENT)
-  ami = data.aws_ami.AMI.id
+  ami = "${data.aws_ami.AMI.id}"
   instance_type = "${var.INSTANCE_TYPE}"
   user_data = "set hostname ${element(var.COMPONENT, count.index)}"
   tags = {
@@ -12,7 +12,7 @@ resource "aws_route53_record" "roboshop" {
   count = length(var.COMPONENT)
   allow_overwrite = true
   name       = "${element(var.COMPONENT, count.index)}.${data.aws_route53_zone.jithendar.name}"
-  zone_id    = data.aws_route53_zone.jithendar.zone_id
+  zone_id    = "${data.aws_route53_zone.jithendar.zone_id}"
   type       = "A"
   ttl        = "300"
   records    = ["${element(aws_instance.instance, count.index)}".private_ip]
