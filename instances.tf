17b43e73fcfd876dbdeb83ed0ddf6ad61f965ba0
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 10:55:09 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "instance" {
-  #count = "${length(var.COMPONENT)}"
-  count = length(var.COMPONENT)
+  count = "${length(var.COMPONENT)}"
   ami = data.aws_ami.AMI.id
   instance_type = "${var.INSTANCE_TYPE}"
   user_data = "set hostname ${element(var.COMPONENT, count.index)}"
   tags = {
-    Name = "${element(var.COMPONENT, count.index)}"-"${var.ENV}"
+    Name = "${element(var.COMPONENT, count.index)}-${var.ENV}"
   }
 }
 
