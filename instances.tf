7286e70087b6e6ce23ccbabd0d38fb2b2c81ccb5
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 13:55:55 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -17,7 +17,8 @@ output "Component" {
 }
 
 resource "aws_route53_record" "roboshop" {
-  for_each = toset(var.COMPONENT)
+  #for_each = toset(var.COMPONENT)
+  count = length(var.COMPONENT)
   allow_overwrite = true
   zone_id    = data.aws_route53_zone.jithendar.zone_id
   name       = each.value.data.aws_route53_zone.jithendar.name
