411a8eaa87f2ba99f49eea7c661c33dcc26467dd
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 12:26:35 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -11,14 +11,12 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "roboshop" {
   dynamic route {
-    count      = length(var.COMPONENT)
     for_each   = var.COMPONENT
-    #iterator   = var.COMPONENT
     zone_id    = data.aws_route53_zone.jithendar.zone_id
-    name       = "${element(var.COMPONENT, index)}".data.aws_route53_zone.jithendar.name
+    name       = "${element(var.COMPONENT)}".data.aws_route53_zone.jithendar.name
     type       = "A"
     ttl        = "300"
-    records    = "${element(aws_instance.instance, index)}".private_ip
+    records    = "${element(aws_instance.instance)}".private_ip
 }
 }
 
