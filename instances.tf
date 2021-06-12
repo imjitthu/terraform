d742339b5fc6dfd681d912740efca09d2fbb5d4f
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 17:14:07 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -10,8 +10,8 @@ resource "aws_instance" "instance" {
 
 resource "null_resource" "make_inv" {
   count   = length(aws_instance.instance)
-  output "inv_file" {
-  value = "${element(aws_instance.instance, count.index).private_ip} ${element(var.COMPONENT, count.index)}"
+  provisioner "local-exec" {
+    command = "echo ${element(aws_instance.instance, count.index).private_ip} ${element(var.COMPONENT, count.index)} >> inv; git clone https://github.com/imjitthu/Ansible.git; ansible-playbook -i inv Ansible/roboshop.yml"
   }
 }
 
