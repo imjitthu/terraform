30a4b9c2299827d961cc56aa6d4839e5de97b54c
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 09:46:26 +0530

    roboshop

--- instances.tf
+++ instances.tf
@@ -5,19 +5,6 @@ resource "aws_instance" "instance" {
   user_data = "set hostname ${element(var.COMPONENT, count.index)}"
   tags = {
     #Name = "${var.ENV}"-"${var.COMPONENT.name}"
-    Name = "${element(var.COMPONENT, count.index)}"-"${var.ENV}"
+    Name = "${element(var.COMPONENT, count.index)}" + "${var.ENV}"
   }
 }
-
-#cidr_block = "${element(var.SUBNET_CIDR, count.index)}"
-#Name = "${element(var.COMPONENT, count.index)}"-"${var.ENV} + 1"
-
-# resource "aws_spot_instance_request" "instance" {
-#   ami = data.aws_ami.AMI.id
-#   spot_price = "0.03"
-#   instance_type = "${var.INSTANCE_TYPE}"
-#   user_data = "set hostname ${element(var.COMPONENT, count.index)}"
-#   tags = {
-#     Name = "name"
-#   }
-# }
