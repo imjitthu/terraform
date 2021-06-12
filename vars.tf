ac5f4078a2971c78ff19d00def9e80b6726b8fd3
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-12 09:44:06 +0530

    roboshop

--- vars.tf
+++ vars.tf
@@ -1,18 +1,4 @@
-variable "COMPONENT" {
-    default = [
-        "mysql",
-        "mongodb",
-        "redis",
-        "rabbitmq",
-        "cart",
-        "catalogue",
-        "shipping",
-        "payment",
-        "user",
-        "frontend"
-    ]
-    }
-    
+variable "COMPONENT" {}
 variable "INSTANCE_TYPE" {}
 variable "REGION" {}
 variable "ENV" {}
