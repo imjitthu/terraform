7813508a0e4ee644f131a74521827066f69077e9
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 11:20:03 +0530

    shared library

--- instances.tf
+++ instances.tf
@@ -10,14 +10,14 @@ resource "aws_instance" "instance" {
 
 resource "null_resource" "make_inv" {
   provisioner "local-exec" {
-    command = "rm -rf /tmp/inv"
+    command = "rm -rf ./inv"
   }
 }
 
 resource "null_resource" "make_inv" {
   count   = length(aws_instance.instance)
   provisioner "local-exec" {
-    command = "echo ${element(aws_instance.instance, count.index).private_ip} ${element(var.COMPONENT, count.index)} >> /tmp/inv"
+    command = "echo ${element(aws_instance.instance, count.index).private_ip} ${element(var.COMPONENT, count.index)} >> ./inv"
   }
 }
 
