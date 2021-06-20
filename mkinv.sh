dcbd8bbe5120e1e0e5ff447ca479c97fff4adff0
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-06-20 17:59:15 +0530

    roles updated

--- mkinv.sh
+++ mkinv.sh
@@ -1,8 +1,8 @@
->inv
+>inv.txt
 for component in frontend-env catalogue-env cart-env user-env shipping-env payment-env mysql-env mongo-env redis-env rabbitmq-env; do
   IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq '.Reservations[].Instances[].PrivateIpAddress' |xargs)
   if [ -n "${IP}" ]; then
-    echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv
+    echo $IP component=${component} ansible_user=root ansible_password=DevOps321 >>inv.txt
   fi
 done
 
